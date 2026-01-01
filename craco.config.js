88122c073047e7e1bc4722c75b9c0fa04c885c5e
s-zuni <zxzx729@gmail.com>
2026-01-01 21:12:00 +0900

    될거지?

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
+  // Craco config - we let postcss.config.js handle Tailwind
 }
