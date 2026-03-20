4a11381f3c8c0d2a87f4786b74708c858d1549d2
s-zuni <zxzx729@gmail.com>
2026-03-20 12:47:52 +0900

    2

--- check_schema.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://tffvsyarxfujmvbqlutr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZnZzeWFyeGZ1am12YnFsdXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMzU0OTEsImV4cCI6MjA4MjkxMTQ5MX0.7ctb_C-BJN_WTNi_yqaQllFY0oVARqsvSjQkte_M-yo');
-
-async function check() {
-  const { data, error } = await supabase.from('profiles').select('*').limit(1);
-  console.log('Profiles data:', data);
-  if (error) console.error('Profiles error:', error);
-}
-check();
