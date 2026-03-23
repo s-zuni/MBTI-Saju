75f4a4fe36043e206ea9650e9437c2ec26b9dced
s-zuni <zxzx729@gmail.com>
2026-03-23 21:57:58 +0900

    3

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -26,8 +26,7 @@ export const supabase = createClient(
             persistSession: true,
             autoRefreshToken: true,
             detectSessionInUrl: true,
-            flowType: 'pkce',
-            storage: typeof window !== 'undefined' ? window.localStorage : undefined
+            flowType: 'pkce'
         }
     }
 );
