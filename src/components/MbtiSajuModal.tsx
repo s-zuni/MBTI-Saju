c2bbf3c087ec382f1357d6c274fa97b19eb5e56d
s-zuni <zxzx729@gmail.com>
2026-03-23 14:11:46 +0900

    2

--- src/components/MbtiSajuModal.tsx
+++ src/components/MbtiSajuModal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { supabase } from '../supabaseClient';
-import { Loader2, Sparkles, Brain, ScrollText, Zap, Share2, Download, Calendar, Layers, Users } from 'lucide-react';
+import { Loader2, Sparkles, Brain, ScrollText, Zap, Share2, Download, Calendar, Layers } from 'lucide-react';
 import { SERVICE_COSTS, SERVICE_NAMES } from '../config/creditConfig';
 import ServiceNavigation, { ServiceType } from './ServiceNavigation';
 
